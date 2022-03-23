be8081e6260486f2c3303ebc8317e2ca01842e91
François Voron <fvoron@gmail.com>
2022-03-23 18:16:48 +0100

    Add FastAPI minimal example

---
+++ examples/python/fastapi/minimal.py
@@ -0,0 +1,27 @@
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from fief_client import FiefAccessTokenInfo, FiefAsync
+from fief_client.integrations.fastapi import FiefAuth
+
+fief = FiefAsync(  # (1)!
+    "https://example.fief.dev",
+    "YOUR_CLIENT_ID",
+    "YOUR_CLIENT_SECRET",
+)
+
+scheme = OAuth2AuthorizationCodeBearer(  # (2)!
+    "https://example.fief.dev/authorize",
+    "https://example.fief.dev/api/token",
+    scopes={"openid": "openid", "offline_access": "offline_access"},
+)
+
+auth = FiefAuth(fief, scheme)  # (3)!
+
+app = FastAPI()
+
+
+@app.get("/user")
+async def get_user(
+    access_token_info: FiefAccessTokenInfo = Depends(auth.current_user()),  # (4)!
+):
+    return access_token_info

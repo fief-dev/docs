2abdbd5f896d6a061192f9a3996a37d812c1f93b
François Voron <fvoron@gmail.com>
2022-03-24 12:19:15 +0100

    Complete FastAPI examples

---
+++ examples/python/fastapi/scope.py
@@ -0,0 +1,29 @@
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from fief_client import FiefAccessTokenInfo, FiefAsync
+from fief_client.integrations.fastapi import FiefAuth
+
+fief = FiefAsync(
+    "https://example.fief.dev",
+    "YOUR_CLIENT_ID",
+    "YOUR_CLIENT_SECRET",
+)
+
+scheme = OAuth2AuthorizationCodeBearer(
+    "https://example.fief.dev/authorize",
+    "https://example.fief.dev/api/token",
+    scopes={"openid": "openid", "offline_access": "offline_access"},
+)
+
+auth = FiefAuth(fief, scheme)
+
+app = FastAPI()
+
+
+@app.get("/user")
+async def get_user(
+    access_token_info: FiefAccessTokenInfo = Depends(
+        auth.current_user(["openid", "required_scope"])
+    ),
+):
+    return access_token_info
---
+++ examples/python/fastapi/web.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+from fastapi import Depends, FastAPI, HTTPException, Query, Request, Response, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.security import APIKeyCookie
+from fief_client import FiefAccessTokenInfo, FiefAsync
+from fief_client.integrations.fastapi import FiefAuth
+
+
+class CustomFiefAuth(FiefAuth):  # (1)!
+    client: FiefAsync
+
+    async def get_unauthorized_response(self, request: Request, response: Response):
+        redirect_uri = request.url_for("auth_callback")  # (2)!
+        auth_url = await self.client.auth_url(redirect_uri, scope=["openid"])  # (3)!
+        raise HTTPException(
+            status_code=status.HTTP_307_TEMPORARY_REDIRECT,  # (4)!
+            headers={"Location": auth_url},
+        )
+
+
+fief = FiefAsync(  # (5)!
+    "https://example.fief.dev",
+    "YOUR_CLIENT_ID",
+    "YOUR_CLIENT_SECRET",
+)
+
+SESSION_COOKIE_NAME = "user_session"
+scheme = APIKeyCookie(name=SESSION_COOKIE_NAME, auto_error=False)  # (6)!
+auth = CustomFiefAuth(fief, scheme)  # (7)!
+app = FastAPI()
+
+
+@app.get("/auth-callback", name="auth_callback")  # (8)!
+async def auth_callback(request: Request, response: Response, code: str = Query(...)):
+    redirect_uri = request.url_for("auth_callback")
+    tokens, _ = await fief.auth_callback(code, redirect_uri)  # (9)!
+
+    response = RedirectResponse(request.url_for("protected"))  # (10)!
+    response.set_cookie(  # (11)!
+        SESSION_COOKIE_NAME,
+        tokens["access_token"],
+        expires=int(datetime.now().timestamp() + tokens["expires_in"]),
+        httponly=True,  # (12)!
+        secure=False,  # ❌ Set this to `True` in production (13)!
+    )
+
+    return response
+
+
+@app.get("/protected", name="protected")
+async def protected(
+    access_token_info: FiefAccessTokenInfo = Depends(auth.current_user()),  # (14)!
+):
+    return HTMLResponse(
+        f"<h1>You are authenticated. Your user ID is {access_token_info['id']}</h1>"
+    )

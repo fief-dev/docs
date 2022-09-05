01b8ed28ac04a9126d181f2fcef167e469359a68
François Voron <fvoron@gmail.com>
2022-09-05 08:25:42 +0200

    Add callout to the FastAPI API example to change the base URL on the OpenAPI security schema
    See https://github.com/fief-dev/fief/discussions/68 for background

--- examples/python/fastapi/api.py
+++ examples/python/fastapi/api.py
@@ -10,18 +10,18 @@ fief = FiefAsync(  # (1)!
 )
 
 scheme = OAuth2AuthorizationCodeBearer(  # (2)!
-    "https://example.fief.dev/authorize",
-    "https://example.fief.dev/api/token",
+    "https://example.fief.dev/authorize",  # (3)!
+    "https://example.fief.dev/api/token",  # (4)!
     scopes={"openid": "openid", "offline_access": "offline_access"},
 )
 
-auth = FiefAuth(fief, scheme)  # (3)!
+auth = FiefAuth(fief, scheme)  # (5)!
 
 app = FastAPI()
 
 
 @app.get("/user")
 async def get_user(
-    access_token_info: FiefAccessTokenInfo = Depends(auth.authenticated()),  # (4)!
+    access_token_info: FiefAccessTokenInfo = Depends(auth.authenticated()),  # (6)!
 ):
     return access_token_info

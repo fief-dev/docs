e568d92b318a5fb0af050a04bbc13c251d7851c5
François Voron <fvoron@gmail.com>
2022-09-11 14:23:37 +0200

    Improve encryption key generator implementation

--- docs/assets/javascripts/fernet.js
+++ docs/assets/javascripts/fernet.js
@@ -1,50 +1,21 @@
-const clipboardIcon = '<svg style="width:16px;height:16px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3" /></svg>';
-
-const clipboardCheckIcon = '<svg style="width:16px;height:16px" viewBox="0 0 24 24"><path fill="currentColor" d="M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3Z" /></svg>';
-
 window.addEventListener('DOMContentLoaded', (event) => {
-  const fernetKeyGenerators = document.getElementsByClassName('fernet-key-generator');
-  for (let i = 0; i < fernetKeyGenerators.length; i++) {
-    const fernetKeyGenerator = fernetKeyGenerators.item(i);
+  window.document$.subscribe(() => {
+    const fernetKeyGenerators = document.getElementsByClassName('fernet-key-generator');
+    for (let i = 0; i < fernetKeyGenerators.length; i++) {
+      const fernetKeyGenerator = fernetKeyGenerators.item(i);
 
-    const outputBox = document.createElement('div');
-    outputBox.className = 'output-box';
+      const output = fernetKeyGenerator.getElementsByTagName('code').item(0);
+      const button = fernetKeyGenerator.getElementsByClassName('md-button').item(0);
 
-    const output = document.createElement('input');
-    output.className = 'md-input';
-    output.disabled = true;
-
-    const copyButton = document.createElement('button');
-    copyButton.type = "button"
-    copyButton.className = "clipboard-button md-clipboard md-icon";
-    copyButton.disabled = true;
-
-    outputBox.appendChild(output);
-    outputBox.appendChild(copyButton);
-
-    const button = document.createElement('button');
-    button.type = "button";
-    button.className = 'md-button';
-    button.innerText = 'Generate key';
-
-    fernetKeyGenerator.appendChild(outputBox);
-    fernetKeyGenerator.appendChild(button);
-
-    let key = '';
-    button.addEventListener('click', () => {
-      button.disabled = true;
-      fetch('https://fernet-key-generator.deta.dev').then((response) => {
-        response.text().then((newKey) => {
-          key = newKey;
-          button.disabled = false;
-          copyButton.disabled = false;
-          output.value = key;
+      button.addEventListener('click', () => {
+        button.disabled = true;
+        fetch('https://fernet-key-generator.deta.dev').then((response) => {
+          response.text().then((key) => {
+            button.disabled = false;
+            output.innerText = key;
+          });
         });
       });
-    });
-
-    copyButton.addEventListener('click', () => {
-      navigator.clipboard.writeText(key);
-    });
-  }
+    }
+  });
 });
--- reusables/fernet-key-generator.md
+++ reusables/fernet-key-generator.md
@@ -1,4 +1,8 @@
 !!! tip "Encryption key generator"
-    <div class="fernet-key-generator"></div>
+    <div class="fernet-key-generator">
+        ```
+        ```
+        <button class="md-button">Generate key</button>
+    </div>
 
     The encryption key should be a valid key for the [Fernet encryption algorithm](https://github.com/fernet/spec/). Use the generator above to get yours! **Keep it secret**.

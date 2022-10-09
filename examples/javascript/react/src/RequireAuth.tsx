54566f1767cfc5258ac89fda5453c932bb676310
François Voron <fvoron@gmail.com>
2022-10-09 11:27:18 +0200

    Update React example for React 18+

--- examples/javascript/react/src/RequireAuth.tsx
+++ examples/javascript/react/src/RequireAuth.tsx
@@ -1,7 +1,7 @@
 import { useFiefAuth, useFiefIsAuthenticated } from '@fief/fief/react';
 import React, { useEffect } from 'react';
 
-const RequireAuth: React.FunctionComponent = ({ children }) => {
+const RequireAuth: React.FunctionComponent<React.PropsWithChildren> = ({ children }) => {
   const fiefAuth = useFiefAuth();  // (1)!
   const isAuthenticated = useFiefIsAuthenticated(); // (2)!
 
--- examples/javascript/react/src/index.tsx
+++ examples/javascript/react/src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

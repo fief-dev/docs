b17a24166359998737e07734142982742177581c
fief-dev-ci <ci@fief.dev>
2025-04-26 11:58:55 +0000

    Deployed da8f54d to dev with MkDocs 1.6.1 and mike 2.1.3

---
+++ dev/assets/javascripts/fernet.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  window.document$.subscribe(() => {
+    const fernetKeyGenerators = document.getElementsByClassName('fernet-key-generator');
+    for (let i = 0; i < fernetKeyGenerators.length; i++) {
+      const fernetKeyGenerator = fernetKeyGenerators.item(i);
+
+      const output = fernetKeyGenerator.getElementsByTagName('code').item(0);
+      const button = fernetKeyGenerator.getElementsByClassName('md-button').item(0);
+
+      button.addEventListener('click', () => {
+        button.disabled = true;
+        fetch('https://fernetgenerator-2-c8847054.deta.app/').then((response) => {
+          response.text().then((key) => {
+            button.disabled = false;
+            output.innerText = key;
+          });
+        });
+      });
+    }
+  });
+});
